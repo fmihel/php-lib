a392c802235073f75cd02ee6eb731b5c127f2cf0
fmihel <fmihel@gmail.com>
2019-10-02 12:52:45 +0300

    fix import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dom = require('./source/dom');
 const react = require('./source/react');
-const JX = require('./source/jx');
-const ut = require('./source/ut');
+const JX = require('./source/jx').default;
+const ut = require('./source/ut').default;
 
 module.exports = {
     ...dom,
