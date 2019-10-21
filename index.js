57e837fbf226d1abc01ea2f6dd987c3657bc6778
fmihel <fmihel@gmail.com>
2019-10-21 12:40:18 +0300

    add ut

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const dom = require('./source/dom');
 const react = require('./source/react');
 const JX = require('./source/jx').default;
+const ut = require('./source/ut').default;
 
 module.exports = {
     ...dom,
     ...react,
     JX,
+    ut,
 };
