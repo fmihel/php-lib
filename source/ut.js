63c105e388bc6d0090bf303a18909cce43adbb2f
fmihel <fmihel@gmail.com>
2020-03-02 15:00:26 +0300

    add ut.onChangeLocation

--- source/ut.js
+++ source/ut.js
@@ -1,9 +1,49 @@
 import ut from 'fmihel-lib';
 
 const _ut = {
+    _changeLocation: {
+        list: [],
+        observer: undefined,
+        pathname: false,
+    },
     /** пустышка  */
     test() {
         return 'test';
+    },
+    /**
+     * событие на изменение маршрута броузера ( в пределах страницы )
+     * @param {function} callback function
+     * @return {function} unregistered callback
+    */
+    onChangeLocation(callback) {
+        const cl = _ut._changeLocation;
+
+        if (cl.observer === undefined) {
+            cl.observer = new MutationObserver(() => {
+                if (window.location.pathname !== cl.pathname) {
+                    cl.pathname = window.location.pathname;
+                    cl.list.forEach((f) => {
+                        try {
+                            f(window.location);
+                        } catch (e) {
+                            console.error(e, f);
+                        }
+                    });
+                }
+            });
+            cl.observer.observe(document.querySelector('body'), { attributes: true, childList: true, subtree: true });
+        }
+        cl.list.push(callback);
+        return () => {
+            let idx = -1;
+            for (let i = 0; i < cl.list.length; i++) {
+                if (cl.list[i] === callback) {
+                    idx = i;
+                    break;
+                }
+            }
+            if (idx > -1) cl.list.splice(idx, 1);
+        };
     },
 };
 
