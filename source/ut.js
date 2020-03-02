d29cc8ea475732319dc0a697ef4fccec2fa32c17
fmihel <fmihel@gmail.com>
2020-03-02 16:48:05 +0300

    call ut.onChangeLocation(undefined) recall all callback functions

--- source/ut.js
+++ source/ut.js
@@ -5,6 +5,19 @@ const _ut = {
         list: [],
         observer: undefined,
         pathname: false,
+        update(forced = false) {
+            const cl = _ut._changeLocation;
+            if ((forced) || (window.location.pathname !== cl.pathname)) {
+                cl.pathname = window.location.pathname;
+                cl.list.forEach((f) => {
+                    try {
+                        f(window.location);
+                    } catch (e) {
+                        console.error(e, f);
+                    }
+                });
+            }
+        },
     },
     /** пустышка  */
     test() {
@@ -12,38 +25,32 @@ const _ut = {
     },
     /**
      * событие на изменение маршрута броузера ( в пределах страницы )
-     * @param {function} callback function
-     * @return {function} unregistered callback
+     * @param {function|undefined} callback function if callback === undefined then call doChangeLocation forced
+     * @return {function|undefined} unregistered callback
     */
+    // eslint-disable-next-line consistent-return
     onChangeLocation(callback) {
         const cl = _ut._changeLocation;
 
         if (cl.observer === undefined) {
-            cl.observer = new MutationObserver(() => {
-                if (window.location.pathname !== cl.pathname) {
-                    cl.pathname = window.location.pathname;
-                    cl.list.forEach((f) => {
-                        try {
-                            f(window.location);
-                        } catch (e) {
-                            console.error(e, f);
-                        }
-                    });
-                }
-            });
+            cl.observer = new MutationObserver(() => { cl.update(); });
             cl.observer.observe(document.querySelector('body'), { attributes: true, childList: true, subtree: true });
         }
-        cl.list.push(callback);
-        return () => {
-            let idx = -1;
-            for (let i = 0; i < cl.list.length; i++) {
-                if (cl.list[i] === callback) {
-                    idx = i;
-                    break;
+        if (callback === undefined) {
+            cl.update(true);
+        } else {
+            cl.list.push(callback);
+            return () => {
+                let idx = -1;
+                for (let i = 0; i < cl.list.length; i++) {
+                    if (cl.list[i] === callback) {
+                        idx = i;
+                        break;
+                    }
                 }
-            }
-            if (idx > -1) cl.list.splice(idx, 1);
-        };
+                if (idx > -1) cl.list.splice(idx, 1);
+            };
+        }
     },
 };
 
