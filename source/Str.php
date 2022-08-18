9b1fe1b366399b4e83d8fe64bb060e48caf6c11f
fmihel <fmihel76@gmail.com>
2022-08-18 13:01:10 +0300

    add Str::translit

--- source/Str.php
+++ source/Str.php
@@ -1,6 +1,11 @@
 <?php
 namespace fmihel\lib;
 
+
+define('STR_TRANSLIT_RUS',str_split(mb_convert_encoding('абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ _-','cp1251','utf-8'),1));
+define('STR_TRANSLIT_ENG',str_split(mb_convert_encoding("abvgdeegziyklmnoprstufhchssuiueuyABVGDEEGZIYKLMNOPRSTUFHCHSSUIUEUY___",'cp1251','utf-8'),1));
+ 
+    
 class Str {
     /**  случайная строка длиной $count (начинается с буквы, всегда загланые буквы и цифры) */
     public static function random(int $count){
@@ -18,5 +23,27 @@ class Str {
 
         return $result;
     }
+    /**  транслитирация для строки
+     *   разрешены все буквы, цифры и знак "_" , пробел и "-" заменяются на "_"
+    */
+    public static function translit(string $s):string{
+       
+       $s   = str_split(mb_convert_encoding($s,'cp1251','utf-8'),1);
+       $len = count($s);
+
+       $out = '';
+       for($i=0;$i<$len;$i++){
+           $code = ord($s[$i]);
+           if ( ($code>=97 && $code<=122) || ($code>=65 && $code<=90) || ($code>=48 && $code<=57)){
+               $out.=$s[$i];
+           }else{
+               $pos = array_search($s[$i],STR_TRANSLIT_RUS);
+               if ($pos!==false){
+                   $out.=STR_TRANSLIT_ENG[$pos];
+               };
+           };
+       };
+       return $out;
+   }
 }
 ?>
--- tests/StrTest.php
+++ tests/StrTest.php
@@ -6,9 +6,37 @@ use fmihel\lib\Str;
 
 
 final class StrTest extends TestCase{
+    
     public function test_random(){
         $value = Str::random(13);
         //error_log(print_r($value,true));
         self::assertTrue( strlen($value) === 13 );
     }
+
+    public function test_translit(){
+        $value = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ _-';
+        $out = 'abvgdeegziyklmnoprstufhchssuiueuyABVGDEEGZIYKLMNOPRSTUFHCHSSUIUEUY___';
+        self::assertTrue( Str::translit($value) === $out );
+
+        $value = 'русский';
+        $out = 'russkiy';
+        self::assertTrue( Str::translit($value) === $out );
+
+        $value = '123Я';
+        $out = '123Y';
+        self::assertTrue( Str::translit($value) === $out );
+
+        $value = '..-+ ';
+        $out = '__';
+        self::assertTrue( Str::translit($value) === $out );
+
+        $value = 'english';
+        $out = 'english';
+        self::assertTrue( Str::translit($value) === $out );
+
+        $value = '&^+#$@%!.,';
+        $out = '';
+        self::assertTrue( Str::translit($value) === $out );
+    }
+
 };
